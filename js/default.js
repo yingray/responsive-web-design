31ae0c13827154533771b60f04e6055d9e420d01
Yingray <yingray.lu@fuhu.com>
2017-01-13 16:55:58 +0800

    updated

---
+++ js/default.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    
+    $(".navbar a").on('click', function (event) {
+        console.log(this.hash)
+        if(this.hash !== '') {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 40
+            }, 900, function () {
+                window.location.hash = hash;
+            })
+        }
+
+    })
+});
+
+$(window).scroll(function() {
+    $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+            $(this).addClass("slide");
+        }
+    });
+});
